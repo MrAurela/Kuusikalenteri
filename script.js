2715ff60177377501ee644b4762c6dce036db376
MrAurela <miroaurela@gmail.com>
2025-11-30 18:10:01 +0200

    Display image from drive based on button id

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 import { PUZZLES } from "./assets.js";
 
-const id = PUZZLES["p1"];
-const url = `https://lh3.googleusercontent.com/d/${id}=w1000`;
 
-document.getElementById("test").src = url;
+
+for (let i=1; i<=24; i++) {
+    let buttonId = "button" + i.toString();
+    document.getElementById(buttonId).onclick = function() { ShowPuzzle(i.toString()); }
+}
+
+function ShowPuzzle(id) {
+    const puzzleId = PUZZLES["p"+id.toString()];
+    const url = `https://lh3.googleusercontent.com/d/${puzzleId}=w1000`;
+
+    document.getElementById("test").src = url;
+}
